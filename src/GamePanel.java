69eb59c821dbce2bdff0e0603362f247899c70fc
Ghita Florin <ghitafm94@gmail.com>
2022-12-06 15:10:20 +0200

    5.Game Over!!!

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,7 +12,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
-	static final int DELAY = 75;
+	static final int DELAY = 70;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
 	int bodyParts = 6;
@@ -41,32 +41,44 @@ public class GamePanel extends JPanel implements ActionListener{
 		timer = new Timer (DELAY,this);
 		timer.start();
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		
+
 		super.paintComponent(g);
-		 draw(g);
+		draw(g);
 	}
-	
+
 	public void draw (Graphics g) {
-		for (int i = 0 ; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
-			g.drawLine(i*UNIT_SIZE, 0 , i*UNIT_SIZE, SCREEN_HEIGHT);
-//			g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,SCREEN_HEIGHT);    ===== MISTO======== - linia urm
-			g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+		if (running ) {
+			for (int i = 0 ; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
+				g.drawLine(i*UNIT_SIZE, 0 , i*UNIT_SIZE, SCREEN_HEIGHT);
+				//			g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,SCREEN_HEIGHT);    ===== MISTO======== - linia urm
+				g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+
+			}
+			g.setColor(Color.red);
+			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+			for (int i = 0 ; i < bodyParts ; i++) {
+				if (i == 0 ) {
+					g.setColor(Color.green);
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+				else {
+					g.setColor(new Color(45,180,0));
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+			g.setColor(Color.red);
+			g.setFont(new Font("Ink Free",Font.BOLD,30));
+			g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
+			FontMetrics metrics = getFontMetrics(g.getFont());
+			g.drawString("Score : "+applesEaten , (SCREEN_WIDTH - metrics.stringWidth("Score : "+applesEaten))/2 ,g.getFont().getSize() ) ;
 			
 		}
-		g.setColor(Color.red);
-		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-		
-		for (int i = 0 ; i < bodyParts ; i++) {
-			if (i == 0 ) {
-				g.setColor(Color.green);
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
-			else {
-				g.setColor(new Color(45,180,0));
-				g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
-			}
+		else {
+			gameOver(g);
 		}
 	}
 	
@@ -101,6 +113,11 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void checkApple() {
+		if((x[0] == appleX) && (y[0] == appleY)) {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
 		
 	}
 	public void checkCollisions () {
@@ -134,6 +151,11 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	public void gameOver(Graphics g) {
 		
+		//Game Over text
+		g.setColor(Color.red);
+		g.setFont(new Font("Ink Free",Font.BOLD,75));
+		FontMetrics metrics = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2 ,SCREEN_HEIGHT/2 ) ;
 	}
 	@Override
 	
@@ -151,7 +173,28 @@ public class GamePanel extends JPanel implements ActionListener{
 	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch(e.getKeyCode()) {
+			case KeyEvent.VK_LEFT:
+				if(direction != 'R') {
+					direction = 'L';
+				}
+				break;
+			case KeyEvent.VK_RIGHT:
+				if(direction != 'L') {
+					direction = 'R';
+				}
+				break;
+			case KeyEvent.VK_UP:
+				if(direction != 'D') {
+					direction = 'U';
+				}
+				break;
+			case KeyEvent.VK_DOWN:
+				if(direction != 'U') {
+					direction = 'D';
+				}
+				break;
+			}
 		}
 	}
 
